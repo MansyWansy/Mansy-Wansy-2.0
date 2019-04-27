171d25ab4015dbf0b8f00b48d57e2a9b5dda795d
Autumn Dawe <skylerraindawe@gmail.com>
2019-04-27 10:43:19 -0400

    fixed a command

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ if(cmd === `${prefix}help`){
  .setTimestamp()
  .setFooter(`Mansy Wansy 2.0`, bot.user.displayAvatarURL)
  .setTitle(`My Commands Are`)
- .setDescription(`hug \n cookie \n tackle \n nom \n dance \n poke \n sleep \n tuckin \n cry \n pout \n cuddle \n handhold \n bite \n lick \n lbite \n slap \n fuck \n spank \n peck \n kiss \n blush \n pet \n avatar \n botinfo \n membercount \m memberinfo \n serverinfo \n \n please be patient this bot is still verry much a work in progress \n i would appreciate any feed back or comments you have about my bot \n thank you have a nice day \n ~Mansy Wansy`)
+ .setDescription(`hug \n cookie \n tackle \n nom \n dance \n poke \n sleep \n tuckin \n cry \n pout \n cuddle \n handhold \n bite \n lick \n lbite \n slap \n spank \n peck \n kiss \n blush \n pet \n avatar \n botinfo \n membercount \m memberinfo \n serverinfo \n \n please be patient this bot is still verry much a work in progress \n i would appreciate any feed back or comments you have about my bot \n thank you have a nice day \n ~Mansy Wansy`)
 
 
 return message.channel.send(helpembed)
